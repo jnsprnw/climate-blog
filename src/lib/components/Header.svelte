55b67b378ebbcd11ed6f78e82eb0e42b77884c2c
Jonas Parnow <6gqrbj@posteo.org>
2024-07-31 16:07:54 +0200

    Clean up

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <header class="mb-16 border-b-2 border-slate-900 pb-16 dark:border-neutral-700">
-	<span class="mb-2 text-xs font-semibold uppercase text-accent">Beta version</span>
 	<a href="/"
 		><h1 class="text-4xl font-semibold leading-9">Artefacts of a<br /> Burning World</h1></a
 	>
