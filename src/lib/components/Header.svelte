f3bf0b461aae386b5bcf755f23ab0da11aaf879d
Jonas Parnow <6gqrbj@posteo.org>
2024-08-02 01:32:11 +0200

    Clean up and hover effect

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <header class="mb-16 border-b-2 border-slate-900 pb-16">
-	<a href="/"
-		><h1 class="text-4xl font-semibold leading-9">Artefacts of a<br /> Burning World</h1></a
-	>
+	<a href="/" class="hover:text-accent text-transition">
+		<h1 class="text-4xl font-semibold leading-9">Artefacts of a<br /> Burning World</h1>
+	</a>
 	<p class="mt-3 max-w-xs font-serif italic leading-5">
 		{SITE_DESCRIPTION}
 	</p>
