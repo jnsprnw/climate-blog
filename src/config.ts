adb505ac504a673125825105a5b2a122c5279c11
Jonas Parnow <6gqrbj@posteo.org>
2024-08-10 00:17:46 +0200

    Typo fix

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = 'Artefacts of a Burning World';
 export const SITE_DESCRIPTION =
-	'Opionated collection of %% articles, films, podcasts and other artefacts related to the climate crisis.';
+	'Opinionated collection of %% articles, films, podcasts and other artefacts related to the climate crisis.';
 export const SITE_AUTHOR = 'Jonas Parnow';
 export const SITE_URL = 'https://climate.jonasparnow.com/';
 export const POSTS_PER_PAGE = 10;
