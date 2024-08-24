003b30288537c651247ed68c87ec1df9ac6efa9e
Jonas Parnow <6gqrbj@posteo.org>
2024-08-24 16:24:46 +0200

    BlogPosting schema added
    #3
    #28

---
+++ src/lib/utils/json-ld.ts
@@ -0,0 +1,32 @@
+import type { BlogPosting } from 'schema-dts';
+import { type Post } from '$types/pocketbase';
+import { getAbsoluteURL } from '$utils/url';
+
+export function serializeSchema(schema: BlogPosting) {
+	return `<script type="application/ld+json">${JSON.stringify(addContext(schema))}</script>`;
+}
+
+function addContext<BlogPosting>(json: BlogPosting) {
+	return {
+		...json,
+		'@context': 'https://schema.org'
+	};
+}
+
+export function createBlogPostingSchema(post: Post): BlogPosting {
+	return {
+		'@type': 'BlogPosting',
+		headline: post.title,
+		url: getAbsoluteURL(`/${post.slug}`),
+		// "description": "Write once, read everywhere",
+		author: {
+			'@type': 'Person',
+			name: 'Jonas Parnow',
+			url: 'https://jonasparnow.com/about'
+		},
+		// "image": "https://csvbase.com/blog-static/excel.png",
+		datePublished: post.published,
+		dateCreated: post.created,
+		dateModified: post.updated
+	};
+}
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import PostSingle from '$lib/components/PostSingle.svelte';
 	import { type Post } from '$types/pocketbase';
+	import { createBlogPostingSchema, serializeSchema } from '$utils/json-ld';
 
 	type Props = {
 		post: Post;
@@ -8,5 +9,9 @@
 
 	const { data }: { data: Props } = $props();
 </script>
+
+<svelte:head>
+	{@html serializeSchema(createBlogPostingSchema(data.post))}
+</svelte:head>
 
 <PostSingle post={data.post} isSingle={true} />
