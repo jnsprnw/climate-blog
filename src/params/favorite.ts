031e603b48e3c0ef7d17e96d05cdd69e9edc07b9
Jonas Parnow <6gqrbj@posteo.org>
2024-12-08 20:51:10 +0100

    Clean up, fixes

--- src/params/favorite.ts
+++ src/params/favorite.ts
@@ -2,5 +2,5 @@ import { STR_FAVORITE } from '$config';
 
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
-	return param === KEY_FAVORITE;
+	return param === STR_FAVORITE;
 }
