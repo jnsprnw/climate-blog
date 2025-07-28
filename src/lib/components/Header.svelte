5f9c3a6b0a8d3b27b6e6df868cad71a35525431d
Jonas Parnow <6gqrbj@posteo.org>
2025-07-28 22:51:09 +0200

    Include about this link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import { getPageDescription } from '$utils/posts';
 	import { getContext } from 'svelte';
-	import { getAbsoluteURL } from '$utils/url';
 	import type { IsModeLight } from '$types/ui';
+	import { getPageDescription } from '$utils/posts';
+	import { getAbsoluteURL } from '$utils/url';
 
 	const isModeLight = getContext<IsModeLight>('isModeLight');
 </script>
 
 <header
-	class="mb-8 pb-8 sm:mb-12 md:mb-16 border-b-2 border-dark dark:border-[#99999A] sm:pb-12 md:pb-16"
+	class="mb-8 pb-8 sm:mb-12 md:mb-16 border-b-2 border-dark dark:border-[#99999A] sm:pb-12 md:pb-16 flex gap-y-4 flex-col md:flex-row justify-between"
 >
-	<a href={getAbsoluteURL('/', isModeLight)} class="hover:text-accent transition-colors">
-		<h1 class="text-3xl leading-none md:text-4xl font-semibold md:leading-9">
-			Artefacts of a<br /> Burning World
-		</h1>
+	<div>
+		<a href={getAbsoluteURL('/', isModeLight)} class="hover:text-accent transition-colors">
+			<h1 class="text-3xl leading-none md:text-4xl font-semibold md:leading-9">
+				Artefacts of a<br /> Burning World
+			</h1>
+		</a>
+		<p class="mt-3 max-w-xs font-serif italic leading-5">
+			{getPageDescription()}
+		</p>
+	</div>
+	<a class="link md:place-self-end text-sm" href={getAbsoluteURL('about', isModeLight)}>
+		About this blog
 	</a>
-	<p class="mt-3 max-w-xs font-serif italic leading-5">
-		{getPageDescription()}
-	</p>
 </header>
